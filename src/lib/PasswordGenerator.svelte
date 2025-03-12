0602b414a77df11aa17681a126ea112ce19ece6b
tnglemongrass <113173292+tnglemongrass@users.noreply.github.com>
2025-03-12 19:50:37 +0000

    Remove images, move heading to parent

--- src/lib/PasswordGenerator.svelte
+++ src/lib/PasswordGenerator.svelte
@@ -22,8 +22,6 @@
   let passwords = generatePasswords(length, count);
 </script>
 
-<h1>Password Generator</h1>
-
 <div class="password-generator-container">
   {#each passwords as password}
     <div class="password">
@@ -33,25 +31,25 @@
 </div>
 
 <style>
-    .password-generator-container {
-      max-width: 800px;
-      margin: 40px auto;
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-      gap: 10px;
-      padding: 20px;
-      background-color: #f9f9f9;
-      border: 1px solid #ddd;
-      border-radius: 10px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-  
-    .password {
-      padding: 10px;
-      background-color: #fff;
-      border: 1px solid #ddd;
-      border-radius: 5px;
-      box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-      text-align: center;
-    }
-  </style>
+  .password-generator-container {
+    max-width: 800px;
+    margin: 40px auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    gap: 10px;
+    padding: 20px;
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .password {
+    padding: 10px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+</style>
